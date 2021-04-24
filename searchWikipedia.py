a97c230f9f33a83dbe2c136d48c0fe3ae8b57520
Dominik <admin@dominik48n.de>
2021-04-24 20:05:29 +0200

    Finish fast wikipedia search script

---
+++ searchWikipedia.py
@@ -0,0 +1,21 @@
+import wikipediaapi
+
+
+def printArticle(description_size):
+    print('Titel des Wikipedia Artikels: {}'.format(page_py.title))
+    print('Link zum Artikel: {}'.format(page_py.fullurl))
+    print('Beschreibung: \n{}'.format(page_py.summary[0:description_size]))
+
+wiki_api = wikipediaapi.Wikipedia('de')
+page_py = wiki_api.page(input('Wikipedia Artikel: '))
+
+if page_py.exists() is False:
+    print('Dieser Wikipedia Artikel konnte nicht gefunden werden.')
+    pass
+
+description_size_string = input('Wie viele Zeichen soll die Beschreibung von Wikipedia dir ausgeben? ')
+
+try:
+    printArticle(int(description_size_string))
+except ValueError as _:
+    print("Bitte gib eine Zahl an.")
