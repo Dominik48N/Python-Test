e2d98961d195a77f14590cab3fcba6b2d1faa9aa
Dominik <admin@dominik48n.de>
2021-04-24 19:50:24 +0200

    Finish the check prime number script

--- checkPrimeNumber.py
+++ checkPrimeNumber.py
@@ -3,16 +3,22 @@ def isNumber():
         return int(inputString)
     except ValueError as _:
         print("Bitte gib eine Zahl an.")
-        return 0
+        return None
 
 
 inputString = input("Zahl: ")
 number = isNumber()
 
-if number != 0:
+if number is not None:
     isPrim = True
 
-    if isPrim:
-        print("{} ist eine Primzahl.".format(number))
+    if number > 1:
+        for i in range(2, number):
+            if (number % i) == 0:
+                print("{} ist keine Primzahl!".format(number))
+                break
+            else:
+                print("{} ist eine Primzahl.".format(number))
+                break
     else:
         print("{} ist keine Primzahl.".format(number))
